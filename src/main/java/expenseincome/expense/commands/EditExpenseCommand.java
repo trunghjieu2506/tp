0c7b693f9d21df5a16125f868cd46acd0c96289d
nicholascxh <02nicholaschong@gmail.com>
2025-03-26 21:54:37 +0800

    Fixed EditExpenseCommand checkstyle

--- src/main/java/expenseincome/expense/commands/EditExpenseCommand.java
+++ src/main/java/expenseincome/expense/commands/EditExpenseCommand.java
@@ -10,7 +10,8 @@ public class EditExpenseCommand extends ExpenseCommand {
     private LocalDate newDate;
     private String newCategory;
 
-    public EditExpenseCommand(int index, String newDescription, double newAmount, LocalDate newDate, String newCategory) {
+    public EditExpenseCommand(int index, String newDescription,
+                              double newAmount, LocalDate newDate, String newCategory) {
         this.index = index;
         this.newDescription = newDescription;
         this.newAmount = newAmount;
