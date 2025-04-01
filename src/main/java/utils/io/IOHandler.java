65fe0d8601c0728d59df186d1d639d3b6200c275
Hudou0420 <1598372655@qq.com>
2025-04-01 19:57:01 +0800

    Added IO util.

---
+++ src/main/java/utils/io/IOHandler.java
@@ -0,0 +1,27 @@
+package utils.io;
+
+import java.util.Scanner;
+
+//this is a class that handles all the input and output
+//it needs a scanner from the main method
+//just throw everything related to System.out.println/input to this class
+//this ensures that our code does not violate OOP rules
+public class IOHandler {
+    private Scanner scanner;
+
+    public IOHandler (Scanner scanner){
+        this.scanner = scanner;
+    }
+
+    public String readInput(){
+        String userInput = scanner.nextLine();
+        //can process the string here
+        return userInput;
+    }
+
+    public void writeOutput(String output){
+
+        //add in necessary stuff
+        System.out.println(output);
+    }
+}
