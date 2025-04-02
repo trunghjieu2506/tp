312fdca884a4c247e57a4c2e4df2022d59b3c0fc
trunghjieu2506 <trunghieu.itw@gmail.com>
2025-04-02 11:55:53 +0800

    Add Finance and DataManger interfaces to integrate data pipeline with the team

---
+++ src/main/java/cashflow/model/interfaces/BudgetDataManager.java
@@ -0,0 +1,8 @@
+package cashflow.model.interfaces;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public interface BudgetDataManager {
+    public ArrayList<Finance> getBudgetList();
+}
---
+++ src/main/java/cashflow/model/interfaces/ExpenseDataManager.java
@@ -0,0 +1,7 @@
+package cashflow.model.interfaces;
+
+import java.util.ArrayList;
+
+public interface ExpenseDataManager {
+    public ArrayList<Finance> getExpenseList();
+}
