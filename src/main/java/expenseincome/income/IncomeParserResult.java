7eb5a470664c4f7b7b4b891e5ce3cbdbeb49874e
nicholascxh <02nicholaschong@gmail.com>
2025-04-06 01:51:13 +0800

    Implement income parser result to determine what message to output

--- src/main/java/expenseincome/income/IncomeParserResult.java
+++ src/main/java/expenseincome/income/IncomeParserResult.java
@@ -1,4 +1,29 @@
 package expenseincome.income;
 
+import expenseincome.income.commands.IncomeCommand;
+
 public class IncomeParserResult {
+    private final IncomeCommand command;
+    private final String errorMessage;
+
+    public IncomeParserResult(IncomeCommand command, String errorMessage) {
+        this.command = command;
+        this.errorMessage = errorMessage;
+    }
+
+    public IncomeCommand getCommand() {
+        return command;
+    }
+
+    public boolean hasCommand() {
+        return command != null;
+    }
+
+    public String getFeedback() {
+        return errorMessage;
+    }
+
+    public boolean hasFeedback() {
+        return errorMessage != null;
+    }
 }
