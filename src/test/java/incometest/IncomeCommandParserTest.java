64f4103fe95d578e162895a4d74abd63d41c1dc7
nicholascxh <02nicholaschong@gmail.com>
2025-04-08 01:40:48 +0800

    Fix Checkstyle

--- src/test/java/incometest/IncomeCommandParserTest.java
+++ src/test/java/incometest/IncomeCommandParserTest.java
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 public class IncomeCommandParserTest {
